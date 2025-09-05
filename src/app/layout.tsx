dcad40b6853086aaee25f6cd49dd57f656651e8e
ccl1889 <jungho595959@gmail.com>
2025-09-05 12:09:43 +0900

    Update logo to pyj.PNG with transparent background and multiply blend mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,19 +38,19 @@ export default function RootLayout({
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-16">
               {/* Logo */}
-                             <Link href="/" className="flex items-center space-x-2">
-                                  <img 
-                    src="/images/pyj.png" 
-                    alt="박영준 법률사무소 로고" 
-                    className="w-8 h-8 object-contain"
-                                         style={{ 
-                       filter: 'brightness(0) invert(1)',
-                       mixBlendMode: 'normal',
-                       backgroundColor: 'transparent'
-                     }}
-                  />
-                 <span className="text-lg font-bold text-white">박영준 법률사무소</span>
-               </Link>
+              <Link href="/" className="flex items-center space-x-2">
+                <img 
+                  src="/images/pyj.PNG" 
+                  alt="박영준 법률사무소 로고" 
+                  className="w-10 h-10 object-contain"
+                  style={{ 
+                    filter: 'brightness(0) invert(1)',
+                    mixBlendMode: 'multiply',
+                    backgroundColor: 'transparent'
+                  }}
+                />
+                <span className="text-lg font-bold text-white">박영준 법률사무소</span>
+              </Link>
               
               {/* Navigation Links */}
               <div className="hidden md:flex items-center space-x-8">
